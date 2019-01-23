d62c1d01e8695c8c4360798b93326c0965bf868d
sudoplox <sk22.3.1999@live.in>
2019-01-23 16:50:14 +0530

    db.data

--- assignment_1/Main.java
+++ assignment_1/Main.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main{
-    public static void main(String args[]){
-        System.out.println("Your first argument is: "+args[0]);
+    public static void main(String args[]) throws IOException{
+        //System.out.println("Your first argument is: "+args[0]);
+        //String field=args[0];
+
+        BufferedReader reader;
+        reader = new BufferedReader(new FileReader("Main.java"));
+        String line = reader.readLine();
+        while (line != null) {
+            System.out.println(line);
+            line = reader.readLine();
+        }
+        reader.close();
     }
 }
