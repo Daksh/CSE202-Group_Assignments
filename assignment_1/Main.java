8960e40663eb8a51b5a13f803e72c989b202b51e
Daksh Shah <daksh17336@iiitd.ac.in>
2019-01-23 23:47:27 +0530

    metadata parsed

--- assignment_1/Main.java
+++ assignment_1/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main{
     public static void main(String args[]) throws IOException{
@@ -15,12 +16,17 @@ public class Main{
         //TODO: Check the validity of Field Name
 
         BufferedReader in = new BufferedReader(new FileReader("metadata.txt"));
-        String line; ;
-        while ((line = in.readLine()) != null) {
-            System.out.println(line);
-            String[] tokens = line.split(",");
+        String line;
+        ArrayList<String[]> metaD = new ArrayList<String[]>(); 
 
+        while ((line = in.readLine()) != null) {
+//            System.out.println(line);
+            String[] tokens = line.split(",");
+            assert tokens.length==3: "Metadata file-format not valid. \nIn one of the rows, found "+tokens.length+" entries instead of 3";
+            metaD.add(tokens);
         }
+        
+//        System.out.println("MetaD has "+metaD.size());
         in.close();
     }
 }
