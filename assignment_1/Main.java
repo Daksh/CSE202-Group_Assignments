739de86693210157bb746e67762a52991a3a666a
Daksh Shah <daksh17336@iiitd.ac.in>
2019-01-24 17:21:41 +0530

    Cleanup

--- assignment_1/Main.java
+++ assignment_1/Main.java
@@ -19,7 +19,6 @@ public class Main{
         ArrayList<String[]> metaD = new ArrayList<String[]>(); 
 
         while ((line = in.readLine()) != null) {
-//            System.out.println(line);
             String[] tokens = line.split(",");
             assert tokens.length==3: "Metadata file-format not valid. \nIn one of the rows, found "+tokens.length+" entries instead of 3";
             System.out.println("Field: "+tokens[0]+", type: "+tokens[1]+", size: "+tokens[2]);
@@ -36,12 +35,10 @@ public class Main{
         }
         
         in = new BufferedReader(new FileReader("db.data"));
-//        String line;
+        
         //Maybe print a header (from MetaD) containing the field titles
         
-        double maxVal=-1*Double.MAX_VALUE;
-    	double minVal=Double.MAX_VALUE;
-    	
+        double maxVal=-1*Double.MAX_VALUE;    	
     	System.out.println("The data file contains these records:");
         while ((line = in.readLine()) != null) {
         	int startIndex=0;
@@ -49,13 +46,8 @@ public class Main{
             	String part = line.substring(startIndex, startIndex+Integer.parseInt(metaD.get(i)[2]));
             	System.out.print(part+"\t");
             	startIndex+=Integer.parseInt(metaD.get(i)[2]);
-//            	System.out.println("'"+metaD.get(i)[1]+"'");
-//            	System.out.println("'"+inputField+"'");
             	if(metaD.get(i)[0].equals(inputField)) {
             		double thisTime = Double.parseDouble(part);
-//            		System.out.println("thisTime: "+thisTime+"\tmaxVal: "+maxVal);
-            		if(thisTime<minVal)
-            			minVal=thisTime;
             		if(thisTime>maxVal)
             			maxVal=thisTime;
             	}
@@ -65,8 +57,6 @@ public class Main{
         in.close();
         System.out.println();
         
-//        System.out.println("MaxVal: "+maxVal+"\t MinVal: "+minVal);
-//        System.out.println("Max Diff: "+(maxVal-minVal));
         System.out.println("Find max value in the field "+inputField);
         if(!presentInMeta) {
         	System.out.println("--- Error: field name not found.");
