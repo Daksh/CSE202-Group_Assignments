56ee390b2806bb16650af8d8be8d16b8afeb36d9
Daksh Shah <daksh17336@iiitd.ac.in>
2019-01-24 16:55:28 +0530

    Value diff printing now. To fix the output

--- assignment_1/Main.java
+++ assignment_1/Main.java
@@ -40,7 +40,34 @@ public class Main{
         }
         
         in = new BufferedReader(new FileReader("db.data"));
+//        String line;
+        //Maybe print a header (from MetaD) containing the field titles
         
+        double maxVal=-1*Double.MAX_VALUE;
+    	double minVal=Double.MAX_VALUE;
+    	
+        while ((line = in.readLine()) != null) {
+        	int startIndex=0;
+        	for(int i=0; i<metaD.size(); i++) {
+            	String part = line.substring(startIndex, startIndex+Integer.parseInt(metaD.get(i)[2]));
+            	System.out.print(part+"\t");
+            	startIndex+=Integer.parseInt(metaD.get(i)[2]);
+//            	System.out.println("'"+metaD.get(i)[1]+"'");
+//            	System.out.println("'"+inputField+"'");
+            	if(metaD.get(i)[0].equals(inputField)) {
+            		double thisTime = Double.parseDouble(part);
+//            		System.out.println("thisTime: "+thisTime+"\tmaxVal: "+maxVal);
+            		if(thisTime<minVal)
+            			minVal=thisTime;
+            		if(thisTime>maxVal)
+            			maxVal=thisTime;
+            	}
+            }
+        	System.out.println();
+        }
         in.close();
+        
+        System.out.println("MaxVal: "+maxVal+"\t MinVal: "+minVal);
+        System.out.println("Max Diff: "+(maxVal-minVal));
     }
 }
